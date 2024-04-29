7e4fec2e5379a9affe3a56c1a97a1621235384f8
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-29 12:20:06 +0900

    ReserveCreateコントローラーのルート追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReserveCreateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware('auth')->group(function () {
+    Route::controller(ReserveCreateController::class)->group(function(){
+        Route::get('reserve/create', 'index')->name('reserveCreate.index');
+    });
+});
