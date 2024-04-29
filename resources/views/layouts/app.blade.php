eb29d91e748e806815a00800e2e808cadbcd91b9
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-29 11:53:54 +0900

    Tailwindのcss適用と背景色

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @vite('resources/css/app.css')
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -18,7 +19,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-amber-500">
             @include('layouts.navigation')
             
 
