1a19ed937287dd856988d30dba421671abfc78c6
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-29 15:01:25 +0900

    storeアクションの作成

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class CourseController extends Controller
 {
@@ -28,7 +30,39 @@ class CourseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required',
+            'description' => 'required'
+        ];
+
+        $messages = [
+            'name.required' => 'コース名は必須です',
+            'description.required' => 'コース内容の説明は必須です'
+        ];
+
+        // バリデータの作成
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        // バリデーションエラー時の処理
+        if ($validator->fails()) {
+            return redirect('course')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        try{
+            DB::beginTransaction();
+            $course = new Course();
+            $course->name = $request->input('name');
+            $course->description = $request->input('description');
+            $course->save();
+            DB::commit();
+            return redirect()->route('course.index')->with(['message' => '「'.$course->name.'」の登録ができました。', 'type' => 'success']);
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error Course Store', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', 'コースの追加に失敗しました');
+        }
     }
 
     /**
