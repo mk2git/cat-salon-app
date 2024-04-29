5b955b8919b6acdd51c031ebb8de66dec49af890
mk2git <86880555+mk2git@users.noreply.github.com>
2024-04-29 22:40:18 +0900

    リレーションシップの追加

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    public function reserveCreates(){
+        return $this->hasMany(ReserveCreate::class);
+    }
 }
