d1cc65537018cf0a8b9d88ccfe11e3cd4b664282
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-01 23:11:16 +0900

    オプション編集の追加

--- resources/views/reserve-option/index.blade.php
+++ resources/views/reserve-option/index.blade.php
@@ -8,40 +8,36 @@
            <x-error-message />
     @endif
 
-    <div class="grid gap-6 grid-cols-2">
+    <div class="grid gap-6 grid-cols-2 mb-5">
       <div>
+        <p class="text-center">オプションの新規登録</p>
         <form action="{{route('reserve-option.store')}}" method="post" class="block w-80 mx-auto mt-3">
           @csrf
-          <label class="block mx-auto mb-3">
-            <span class="block text-sm font-medium text-slate-700">オプション名</span>
-            <input type="text" name="name" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-              invalid:border-pink-500 invalid:text-pink-600
-              focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-            "/>
-          </label>
-          <label class="block mx-auto mb-3">
-            <span class="block text-sm font-medium text-slate-700">料金</span>
-            <input type="number" name="fee" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-              invalid:border-pink-500 invalid:text-pink-600
-              focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-            "/>
-          </label>
-          <label class="block mx-auto mb-6">
-            <span class="block text-sm font-medium text-slate-700">オプション内容</span>
-            <textarea id="message" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="オプション内容を入力してください"></textarea>
-          </label>
+          <x-option-form />
           <button type="submit" class="py-2 px-5 bg-orange-500 text-white font-semibold rounded-full shadow-md hover:bg-orange-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75 block mx-auto">登録</button>
         </form>
       </div>
       <div>
+        <p class="text-center">オプションの編集</p>
         @foreach ($options as $option)
-          
+          <div class="border rounded p-4 mb-3">
+            <form action="{{route('reserve-option.update', $option->id)}}" method="post" class="block mb-3">
+              @csrf
+              @method('put')
+              <x-option-form :option-name="$option->name" :option-fee="$option->fee" :option-desc="$option->description" />
+              <button type="submit" class="py-2 px-5 bg-teal-600 text-white font-semibold rounded-full shadow-md hover:bg-teal-600 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75 block mx-auto">更新</button>
+            </form>
+            <hr>
+            <div class="mt-3 text-right">
+               <form action="{{route('reserve-option.delete', $option->id)}}" method="post">
+                  @csrf
+                  @method('delete')
+                <small>このオプションを削除しますか？</small>&nbsp;&nbsp;
+                <button type="submit" class="py-2 px-5 bg-red-600 text-white font-semibold rounded shadow-md hover:bg-red-800 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75">削除</button>
+              </form>
+            </div>
+          </div>  
         @endforeach
-        オプション内容表示
       </div>
     </div>
   </div>
