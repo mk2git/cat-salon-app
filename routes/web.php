7fb93beaef453ccf0529a3bc5238dd101dba6c43
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-01 14:22:54 +0900

    adminのルート追加

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+Route::get('/admin/dashboard', function () {
+    return view('admin-dashboard');
+})->middleware(['auth', 'can:admin', 'verified'])->name('admin.dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
