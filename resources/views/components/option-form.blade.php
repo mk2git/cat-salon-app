ac9afd8976822f96675166849373e6815f4f92a1
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-01 18:28:48 +0900

    optionのフォーム用のcomponent作成

---
+++ resources/views/components/option-form.blade.php
@@ -0,0 +1,47 @@
+@if (isset($optionName))
+  <label class="block mx-auto mb-3">
+    <span class="block text-sm font-medium text-slate-700">オプション名</span>
+    <input type="text" name="name" value="{{$optionName}}" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <label class="block mx-auto mb-3">
+    <span class="block text-sm font-medium text-slate-700">料金</span>
+    <input type="number" name="fee" value="{{$optionFee}}" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <label class="block mx-auto mb-6">
+    <span class="block text-sm font-medium text-slate-700">オプション内容</span>
+    <textarea id="message" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="オプション内容を入力してください">{{$optionDesc}}</textarea>
+  </label> 
+@else
+  <label class="block mx-auto mb-3">
+    <span class="block text-sm font-medium text-slate-700">オプション名</span>
+    <input type="text" name="name" value="" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <label class="block mx-auto mb-3">
+    <span class="block text-sm font-medium text-slate-700">料金</span>
+    <input type="number" name="fee" value="" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+    "/>
+  </label>
+  <label class="block mx-auto mb-6">
+    <span class="block text-sm font-medium text-slate-700">オプション内容</span>
+    <textarea id="message" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="オプション内容を入力してください"></textarea>
+  </label> 
+@endif
