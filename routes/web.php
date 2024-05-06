a81832a89dadc1d853ec0f2052dba377974862bc
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-06 23:30:44 +0900

    現時点で不要な箇所のコメントアウト

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ReserveOptionController;
 use App\Http\Controllers\ReserveCreateController;
+use App\Http\Controllers\CalendarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::get('/admin/dashboard', function () {
-    return view('admin-dashboard');
-})->middleware(['auth', 'can:admin', 'verified'])->name('admin.dashboard');
+// Route::get('/admin/dashboard', function () {
+//     return view('admin-dashboard');
+// })->middleware(['auth', 'can:admin', 'verified'])->name('admin.dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -42,4 +43,6 @@ Route::middleware('auth')->group(function () {
     Route::post('option', [ReserveOptionController::class, 'store'])->name('reserve-option.store');
     Route::put('option/{id}', [ReserveOptionController::class, 'update'])->name('reserve-option.update');
     Route::delete('option/{id}', [ReserveOptionController::class, 'destroy'])->name('reserve-option.delete');
+
+    // Route::get('reserve/calendar', [CalendarController::class, 'index']);
 });
