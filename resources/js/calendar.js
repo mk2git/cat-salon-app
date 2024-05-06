a8edec43b095dfce17e29fdcfdde36e0b89ec99f
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-06 23:29:07 +0900

    現時点で必要ないためコメントアウト

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,32 +1,33 @@
-import { Calendar } from '@fullcalendar/core';
-import interactionPlugin from '@fullcalendar/interaction';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
+// FullCalendarを利用する際に必要
+// import { Calendar } from '@fullcalendar/core';
+// import interactionPlugin from '@fullcalendar/interaction';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
+// import listPlugin from '@fullcalendar/list';
 
-document.addEventListener('DOMContentLoaded', function() {
-  const calendarEl = document.getElementById('calendar');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const calendarEl = document.getElementById('calendar');
 
-  if (!calendarEl) {
-    return;
-  }
+//   if (!calendarEl) {
+//     return;
+//   }
 
-  const events = JSON.parse(calendarEl.dataset.events); // data-eventsの値を取得
-  const today = new Date(); // 今日の日付を取得
-  const calendar = new Calendar(calendarEl, {
-    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    locale: 'ja',
-    initialDate: today,
-    navLinks: true, // can click day/week names to navigate views
-    editable: true,
-    dayMaxEvents: true, // allow "more" link when too many events
-    events,
-  });
+//   const events = JSON.parse(calendarEl.dataset.events); // data-eventsの値を取得
+//   const today = new Date(); // 今日の日付を取得
+//   const calendar = new Calendar(calendarEl, {
+//     plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+//     headerToolbar: {
+//       left: 'prev,next today',
+//       center: 'title',
+//       right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+//     },
+//     locale: 'ja',
+//     initialDate: today,
+//     navLinks: true, // can click day/week names to navigate views
+//     editable: true,
+//     dayMaxEvents: true, // allow "more" link when too many events
+//     events,
+//   });
 
-  calendar.render();
-});
+//   calendar.render();
+// });
