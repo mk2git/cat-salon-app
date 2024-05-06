c74a471b0505123851a1034a6be16cd3e920f880
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-06 23:29:24 +0900

    今日の予定を追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,8 +2,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-               
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5">
+               <p>今日の予定</p>
+               <p>---</p>
+            </div>
+
+            <div>
+
             </div>
         </div>
     </div>
