080fd2f7aae90c81c85a7bf7a2da75042a6608cf
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-07 15:17:58 +0900

    alertの色をtypeに応じて変えられるよう修正

--- resources/views/components/alert-message.blade.php
+++ resources/views/components/alert-message.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mx-auto" role="alert">
+<div class="bg-{{$type}}-100 border-l-4 border-{{$type}}-500 text-orange-700 p-4 mx-auto" role="alert">
   <p class="font-bold">{{$message}}</p>
 </div>
