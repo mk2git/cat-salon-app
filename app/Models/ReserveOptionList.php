1320cb64f4ea7c0d818bb515ac80f87ad4c0338a
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-10 16:37:23 +0900

    reserve_option_listsテーブルのモデル作成

---
+++ app/Models/ReserveOptionList.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReserveOptionList extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'reserve_option_id'
+    ];
+}
