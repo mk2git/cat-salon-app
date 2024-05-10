80d9426cdc19413d02188ef7b373dfd433642516
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-10 16:40:26 +0900

    ルート名の修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -45,7 +45,7 @@
                                 現在の予約状況
                             </x-dropdown-link>
                         @else
-                            <x-dropdown-link :href="route('reserve.create')">
+                            <x-dropdown-link :href="route('reserve.index')">
                                 予約する
                             </x-dropdown-link>
                             <x-dropdown-link :href="route('reserveCreate.status')">
