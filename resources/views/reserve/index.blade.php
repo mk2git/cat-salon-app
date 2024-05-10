d069175b14f31a13415d4e4cb4b1e97d593a916b
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-10 16:49:17 +0900

    予約画面のビュー作成

---
+++ resources/views/reserve/index.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+  <div class="container mt-8 mx-auto">
+    <h2 class="text-center text-2xl">どの予約をしますか？</h2>
+    <div class="flex justify-between">
+      <button id="prev" type="button" class="py-2 px-5 bg-green-500 rouded text-white">前の月</button>
+      <button id="next" type="button" class="py-2 px-5 bg-green-500 rouded text-white">次の月</button>
+    </div>
+    <hr>
+    <div id="calendar"></div>
+    <x-reserve-calendar :events="$events" />
+  </div>
+</x-app-layout>
