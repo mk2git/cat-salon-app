86b82f404e9be42e145e6f1f5f2dc4a950e3e585
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-10 16:36:18 +0900

    Reserveコントローラーのcreateアクションのルート追加

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,13 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ReserveOptionController;
 use App\Http\Controllers\ReserveCreateController;
-use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\ReserveController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
 });
-// Route::get('/admin/dashboard', function () {
-//     return view('admin-dashboard');
-// })->middleware(['auth', 'can:admin', 'verified'])->name('admin.dashboard');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -48,5 +46,9 @@ Route::middleware('auth')->group(function () {
     Route::put('option/{id}', [ReserveOptionController::class, 'update'])->name('reserve-option.update');
     Route::delete('option/{id}', [ReserveOptionController::class, 'destroy'])->name('reserve-option.delete');
 
-    // Route::get('reserve/calendar', [CalendarController::class, 'index']);
+    Route::controller(ReserveController::class)->group(function(){
+        Route::get('reserve', 'create')->name('reserve.create');
+    });
+    
+
 });
