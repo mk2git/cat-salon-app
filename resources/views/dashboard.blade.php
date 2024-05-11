3420a2a4f4ef6ad78acc03514faad2b45a9d8bd1
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-11 15:58:48 +0900

    アラートメッセージと該当するユーザーの予約一覧の追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,22 @@
 <x-app-layout>
-
+    @if (session('message'))
+        <x-alert-message :message="session('message')" :type="session('type')" />
+    @endif
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5">
-               <p>今日の予定</p>
-               <p>---</p>
+                @can('admin')
+                  <p>今日の予定</p>
+                @else
+                  <p class="mb-3">予約一覧</p>
+                  <ul class="list-disc list-inside">
+                    @foreach ($reserve_lists as $reserve_list)
+                        @if($reserve_list['user_id'] == Auth::user()->id)
+                            <li class="mb-3"><a href="" class="hover:text-teal-400">{{$reserve_list['date']}}&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve_list['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve_list['course_name']}}</a></li>
+                        @endif
+                    @endforeach
+                  </ul>               
+                @endcan
             </div>
 
             <div>
