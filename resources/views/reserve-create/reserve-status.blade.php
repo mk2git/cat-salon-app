6c7434d6b8d9e496140585f5f00ff34bd3d1139a
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-12 16:09:55 +0900

    現在の予約状況のカレンダーをビューに追加、予約済みマークも追加

--- resources/views/reserve-create/reserve-status.blade.php
+++ resources/views/reserve-create/reserve-status.blade.php
@@ -1,13 +1,16 @@
 <x-app-layout>
   <div class="container m-8 mx-auto">
     <h2 class="text-center mt-6 text-2xl">現在の予約状況</h2>
+    <p class="text-end my-3">
+      <span class="p-1 text-red-700"><i class="fa-solid fa-check"></i></span>・・・予約済み
+    </p>
     <div class="flex justify-between">
       <button id="prev" type="button" class="py-2 px-5 bg-green-500 rouded text-white">前の月</button>
       <button id="next" type="button" class="py-2 px-5 bg-green-500 rouded text-white">次の月</button>
     </div>
     <hr>
     <div id="calendar"></div>
-    <x-calendar :events="$events" />
+    <x-reserve-status-calendar :events="$events" />
   </div>
   
 </x-app-layout>
