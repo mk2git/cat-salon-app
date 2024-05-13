b9ac4eba99412255abef20259c1dd5ebbaa3e41e
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-13 19:24:19 +0900

    storeアクションの追加

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -10,6 +10,8 @@ use App\Models\ReserveOptionList;
 use App\Models\BodyCheck;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class RecordController extends Controller
 {
@@ -56,7 +58,61 @@ class RecordController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'cat_name' => 'required',
+            'cat_species' => 'required',
+            'weight' => 'required',
+            'message' => 'required',
+        ];
+
+        $messages = [
+            'cat_name.required' => '猫の名前は必須です',
+            'cat_species.required' => '猫種は必須です',
+            'weight.required' => '体重の入力は必須です',
+            'message.required' => 'メッセージは必須です'
+        ];
+
+        // バリデータの作成
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        // バリデーションエラー時の処理
+        if ($validator->fails()) {
+            return redirect('record/' .$request->reserve_id)
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+        try{
+            DB::beginTransaction();
+            $body_check = new BodyCheck();
+            $body_check->user_id = $request->input('user_id');
+            $body_check->cat_name = $request->input('cat_name');
+            $body_check->ear = $request->input('ear');
+            $body_check->eye = $request->input('eye');
+            $body_check->hair_loss = $request->input('hair_loss');
+            $body_check->hair_ball =$request->input('hair_ball');
+            $body_check->others =  $request->input('others');
+            $body_check->save();
+
+            $record = new Record();
+            $record->user_id = $request->input('user_id');
+            $record->body_check_id = $body_check->id;
+            $record->reserve_id = $request->input('reserve_id');
+            $record->cat_name = $request->input('cat_name');
+            $record->cat_species = $request->input('cat_species');
+            $record->weight = $request->input('weight');
+            $record->message = $request->input('message');
+            $record->save();
+
+            DB::commit();
+            $message = $request->cat_name;
+            return redirect()->route('dashboard')->with(['message' => '「'.$message.'ちゃん」のカルテが登録されました。', 'type' => 'orange']);
+            
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error Record Store', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', 'カルテの登録に失敗しました');
+        }
+
     }
 
     /**
