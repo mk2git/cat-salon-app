5700671d2bf5e0845f46736a6a0ff5a9ad137ba1
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-13 19:02:02 +0900

    text-centerを追加

--- resources/views/components/error-message.blade.php
+++ resources/views/components/error-message.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+<div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center" role="alert">
   <ul>
     @foreach ($errors->all() as $error)
       <li>
