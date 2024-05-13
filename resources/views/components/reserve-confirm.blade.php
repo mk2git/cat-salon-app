38c4fd8f7af0b27546deaaeb2b7789a2c22f7c71
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-13 17:39:46 +0900

    gap修正

--- resources/views/components/reserve-confirm.blade.php
+++ resources/views/components/reserve-confirm.blade.php
@@ -1,16 +1,16 @@
-<div class=" grid grid-cols-2 gap-6">
+<div class=" grid grid-cols-2 gap-5">
   <div class="text-end">日付：</div>
   <div>{{$reserve['date']}}</div>
 </div>
-<div class=" grid grid-cols-2 gap-6">
+<div class=" grid grid-cols-2 gap-5">
   <div class="text-end">時間：</div>
   <div>{{$reserve['time']}}〜</div>
 </div>
-<div class=" grid grid-cols-2 gap-6">
+<div class=" grid grid-cols-2 gap-5">
   <div class="text-end">コース：</div>
   <div>{{$reserve['course_name']}}</div>
 </div>
-<div class=" grid grid-cols-2 gap-6">
+<div class=" grid grid-cols-2 gap-5">
   <div class="text-end">オプション：</div>
   <div>
     @if ($reserve['option'] == '')
@@ -20,7 +20,7 @@
     @endif  
   </div>
 </div>
-<div class=" grid grid-cols-2 gap-6">
+<div class=" grid grid-cols-2 gap-5">
   <div class="text-end">合計金額：</div>
   <div>&yen;{{$reserve['price']}}</div>
 </div>
