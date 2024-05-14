cb93ffea5c09d529a2c7f4ba4f2eef0ed69a564a
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-14 15:35:43 +0900

    色やpaddingの調整

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
   <p class="m-6 text-2xl border-b w-1/4 text-center">{{$reserve['user_name']}}&nbsp;&nbsp;<small>様</small></p>
-  <div class="grid grid-cols-3 gap-2 p-6 mb-8">
-    <div class="mx-auto p-6 drop-shadow-2xl">
+  <div class="grid grid-cols-3 gap-2 mb-8">
+    <div class="mx-auto p-2 drop-shadow-2xl">
       <div class="border-4 rounded border-teal-600 py-6 px-4 mx-auto">
         <p class="text-center border-b font-bold mb-6">予約内容</p>
         <x-reserve-confirm :reserve="$reserve" />
       </div>
        
     </div>
-    <div class="col-span-2 w-3/4 mx-auto border-2 rounded border-orange-700 bg-orange-700 p-6">
+    <div class="col-span-2 w-3/4 mx-auto border-2 rounded border-amber-400 bg-amber-400 p-6">
       <div class="rounded bg-white p-6">
         <p class="text-center mt-6 font-bold">カルテ</p>
         <hr class="w-1/4 mx-auto">
