57ff6b08ebd48f0cfec0defd1d9153182b749f4e
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-18 21:14:43 +0900

    最後に位置するdivタグをgrid内に移動

--- resources/views/components/record-form.blade.php
+++ resources/views/components/record-form.blade.php
@@ -50,8 +50,8 @@
     ⚪︎その他：
     <input type="text" name="others" class="w-3/4 rounded-md border-b border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value="{{ $recordList['others'] ?? '' }}">
   </div>
-</div>
-<div>
-  <p><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;担当グルーマーからのメッセージ</p>
-  <textarea name="message" cols="30" rows="10" class="w-full rounded-md border-b border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{ $recordList['message'] ?? '' }}</textarea>
+  <div>
+    <p><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;担当グルーマーからのメッセージ</p>
+    <textarea name="message" cols="30" rows="10" class="w-full rounded-md border-b border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{ $recordList['message'] ?? '' }}</textarea>
+  </div>
 </div>
