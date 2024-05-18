0c1e8e49c25a74cadb7312b6633bfa0a157da591
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-18 15:25:46 +0900

    editアクションにuser_id追加、updateアクション作成

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -9,6 +9,7 @@ use App\Models\ReserveOption;
 use App\Models\ReserveOptionList;
 use App\Models\Course;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class CheckoutController extends Controller
 {
@@ -68,6 +69,7 @@ class CheckoutController extends Controller
         $reserve_options = ReserveOptionList::where('reserve_id', $reserve_id->id)->get();
         $reserve_content = [
             'reserve_id' => $reserve_id->id,
+            'user_id' => $reserve_create->reserve->user_id,
             'user_name' => $reserve_create->reserve->user->name,
             'course_id' => $reserve_create->course_id,
             'options' => $reserve_options
@@ -79,9 +81,44 @@ class CheckoutController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        try{
+            DB::beginTransaction();
+            $reserve_create_id = Reserve::where('id', $request->reserve_id)->value('reserve_create_id');
+            $reserve_create = ReserveCreate::find($reserve_create_id);
+            $reserve_create->course_id = $request->input('course_id');
+            $reserve_create->save();
+
+            $existing_reserve_options = ReserveOptionList::where('reserve_id', $request->reserve_id)->get();
+            // 既存のオプションを削除または更新
+            $new_options = collect($request->reserve_option);
+            // dd($new_options);
+            foreach ($existing_reserve_options as $option) {
+                if (!$new_options->contains($option->reserve_option_id)) {
+                    $option->delete();
+                }
+            }
+
+            // 新しいオプションを追加
+            foreach ($new_options as $option_id) {
+                if (!$existing_reserve_options->contains('reserve_option_id', $option_id)) {
+                    ReserveOptionList::create([
+                        'user_id' => $request->input('user_id'),
+                        'reserve_option_id' => $option_id,
+                        'reserve_id' => $request->input('reserve_id')
+                    ]);
+                }
+            }
+            DB::commit();
+            $message = '本日のサロン内容が更新されました。';
+            return view('checkout.checkout-form')->with(['message' => $message, 'type' => 'green']);
+        }catch(\Throwable $th){
+            DB::rollBack();
+            logger('Error Checkout Update', ['message' => $th->getMessage()]);
+            return redirect()->back()->with('error', 'サロン内容の更新に失敗しました');
+        }
+        return view('checkout.checkout-form');
     }
 
     /**
