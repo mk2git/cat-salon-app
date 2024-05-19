23b9e629fc07f13b9c3d182cd4e198c10a144e18
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-19 16:50:32 +0900

    showアクションで予約一覧まで必要なデータを取得

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Record;
+use App\Models\Reserve;
+use App\Models\BodyCheck;
+use App\Models\ReserveOptionList;
 
 class UserController extends Controller
 {
@@ -37,9 +42,32 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $user_name = User::where('id',$id)->value('name');
+        $cats = Record::where('user_id', $id)->select('cat_name', 'cat_species')->distinct()->get();
+
+        $reserves = [];
+        $reserveAndReserveCreateIds = Reserve::where('user_id', $id)->where('status', config('reserve.not yet'))->select('id', 'reserve_create_id')->get();
+
+        $options = ReserveOptionList::where('user_id', $id)->where('status', config('reserve_option_list.not yet'))->get();
+        foreach($reserveAndReserveCreateIds as $reserveAndReserveCreateId){
+            $time = Carbon::createFromFormat('H:i:s', $reserveAndReserveCreateId->reserve_create->time)->format('H:i');
+            $reserve_options = $options->where('reserve_id', $reserveAndReserveCreateId->id);
+            $option_names = $reserve_options->pluck('reserve_option.name')->implode(', ');
+             $reserves[] = [
+            'reserve_create_id' => $reserveAndReserveCreateId->reserve_create_id,
+            'date' => $reserveAndReserveCreateId->reserve_create->date,
+            'time' => $time,
+            'course_name' => $reserveAndReserveCreateId->reserve_create->course->course_name,
+            'option_names' => $option_names
+        ];
+        }
+
+        $records = Record::where('user_id', $id)->get();
+        // $data = 
+
+        return view('user.show', compact('user_name', 'cats', 'reserves'));
     }
 
     /**
