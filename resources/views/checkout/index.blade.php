a15c0a2a82682d5b10282efab149e3ce08ed6ad5
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-20 13:35:45 +0900

    お会計できるものがなかった場合の表示を追加

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -1,8 +1,11 @@
 <x-app-layout>
   <div class="container my-10 mx-auto">
-    <h2 class="text-center text-2xl">どのお会計を行いますか？</h2>
+    <h2 class="text-center text-2xl mb-2">どのお会計を行いますか？</h2>
+    <hr class="w-3/4 mx-auto">
     <div class="mt-6 flex">
+      @php $hasCheckout = false; @endphp
       @foreach ($checkouts as $checkout)
+        @php $hasCheckout = true; @endphp
         <a href="{{route('checkout.edit', $checkout['reserve_id'])}}" class="block p-6 w-1/4 mx-6 shadow-xl hover:bg-gray-100">
           <p><span class="font-bold text-xl">{{$checkout['user_name']}}</span>&nbsp;&nbsp;様</p>
           <hr>
@@ -10,6 +13,9 @@
           <p class="mt-3 text-center">{{$checkout['course_name']}}</p>
         </a>
       @endforeach
+      @if (!$hasCheckout)
+        <p class="text-center mt-10 mx-auto">お会計できるものはありません。</p>
+      @endif
     </div>
   </div>
 </x-app-layout>
