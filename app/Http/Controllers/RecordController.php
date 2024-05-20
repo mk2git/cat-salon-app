17de4e0072b90d21f1d841888042fa661cad2b3f
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-20 23:30:42 +0900

    userRecordsアクション作成

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -141,6 +141,52 @@ class RecordController extends Controller
 
     }
 
+    public function userRecords($user_id)
+    {
+        $reserves = Reserve::where('user_id', $user_id)->where('checkout_status', config('reserve.done'))->orderBy('created_at', 'desc')->get();
+        $contents = [];
+
+        foreach($reserves as $reserve){
+            $record = Record::where('reserve_id', $reserve->id)->first();
+            $body_check_id = Record::where('reserve_id', $reserve->id)->value('body_check_id');
+            $body_check = BodyCheck::find($body_check_id);
+            $course_fee = number_format($reserve->reserve_create->course->fee);
+            $options = ReserveOptionList::where('reserve_id', $reserve->id)->get();
+            $option_prices = $options->pluck('reserve_option.fee')->toArray();
+            $option_price = array_sum($option_prices);
+            $totalFee = $reserve->reserve_create->course->fee + $option_price;
+
+            $subTotal = number_format($totalFee);
+            $tax = 0.1;
+
+            $onlyTax = number_format($totalFee * $tax);
+            $total = number_format($totalFee + ($totalFee * $tax));
+
+            $contents[] = [
+                'user_id' => $reserve->user_id,
+                'reserve_id' => $reserve->id,
+                'date' => $reserve->reserve_create->date,
+                'cat_name' => $record->cat_name,
+                'cat_species' => $record->cat_species,
+                'weight' => $record->weight,
+                'ear' => $body_check->ear,
+                'eye' => $body_check->eye,
+                'hair_loss' => $body_check->hair_loss,
+                'hair_ball' => $body_check->hair_ball,
+                'others' => $body_check->others,
+                'message' => $record->message,
+                'course_name' => $reserve->reserve_create->course->course_name,
+                'course_fee' => $course_fee,
+                'options' => $options,
+                'subTotal' => $subTotal,
+                'onlyTax' => $onlyTax,
+                'total' => $total
+            ];
+        }
+        
+        return view('record.user-records', compact('contents'));
+    }
+
     /**
      * Display the specified resource.
      */
