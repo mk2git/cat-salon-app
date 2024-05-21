ce57d35c38a48068b62d320842c885ddec4cd5ff
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-21 17:05:32 +0900

    色とマージン修正

--- resources/views/reserve-create/reserve-status.blade.php
+++ resources/views/reserve-create/reserve-status.blade.php
@@ -5,11 +5,11 @@
       <span class="p-1 text-red-700"><i class="fa-solid fa-check"></i></span>・・・予約済み
     </p>
     <div class="flex justify-between">
-      <button id="prev" type="button" class="py-2 px-5 bg-green-500 rouded text-white">前の月</button>
-      <button id="next" type="button" class="py-2 px-5 bg-green-500 rouded text-white">次の月</button>
+      <button id="prev" type="button" class="py-2 px-5 bg-green-700 rouded text-white">前の月</button>
+      <button id="next" type="button" class="py-2 px-5 bg-green-700 rouded text-white">次の月</button>
     </div>
     <hr>
-    <div id="calendar"></div>
+    <div id="calendar" class="mx-6"></div>
     <x-reserve-status-calendar :events="$events" />
   </div>
   
