15e014f423aa2b96faff0c5936a8146a6d4bc4b9
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-24 16:24:40 +0900

    カルテと料金詳細をまとめた表示ではなく、サロン履歴の日付やコース、オプションの一覧に修正

--- resources/views/record/user-records.blade.php
+++ resources/views/record/user-records.blade.php
@@ -1,8 +1,23 @@
 <x-app-layout>
-  <p class="mt-10 text-2xl text-center"><i class="fa-regular fa-clipboard"></i>&nbsp;&nbsp;サロン履歴一覧</p>
-  @foreach ($contents as $content)
-    <x-record :content="$content" />
-  @endforeach
-  {{$reserves->links()}}
-  <br>
+  <p class="mt-10 text-2xl text-center mb-6"><i class="fa-regular fa-clipboard"></i>&nbsp;&nbsp;サロン履歴一覧</p>
+  <div class="border rounded p-10 w-1/2 mx-auto">
+    <ul class="mt-6">
+      @php $hasReservationRecords = false; @endphp
+      @foreach ($done_reserve_records as $done_reserve_record)
+        @php $hasReservationRecords = true; @endphp
+        <li class="mb-2">
+          <a href="{{route('user.showRecord', $done_reserve_record['reserve_id'])}}" class="hover:text-blue-500 underline decoration-blue-500 hover:decoration-2">
+            {{$done_reserve_record['date']}}&nbsp;&nbsp;&nbsp;&nbsp;{{$done_reserve_record['course_name']}}
+            @if ($done_reserve_record['options'])
+             （ {{$done_reserve_record['options']}}）
+            @endif
+          </a>
+        </li>
+      @endforeach
+      @if (!$hasReservationRecords)
+        <p>サロン履歴はまだありません</p>
+      @endif
+    </ul>
+    {{$done_reserves->links()}}
+  </div>
 </x-app-layout>
