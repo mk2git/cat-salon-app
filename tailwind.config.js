380efd4281788b5db31bd836762aa5528d63f43b
mk2git <86880555+mk2git@users.noreply.github.com>
2024-05-31 22:20:28 +0900

    色をカスタマイズ

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,25 +11,25 @@ export default {
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+            // fontFamily: {
+            //     sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            // },
             colors: {
-                'emerald': {
-                    '300': 'rgb(110 231 183)',
-                    '400': 'rgb(52 211 153)'
+                yellow: {
+                    300: 'rgb(253 224 71)',
+                    400: 'rgb(250 204 21)'
                 },
-                'yellow': {
-                    '300': 'rgb(253 224 71)',
-                    '400': 'rgb(250 204 21)'
+                emerald: {
+                    300: 'rgb(110 231 183)',
+                    400: 'rgb(52 211 153)'
                 },
-                'blue': {
-                    '300': 'rgb(147 197 253)',
-                    '400': 'rgb(96 165 250)'
+                blue: {
+                    300: 'rgb(147 197 253)',
+                    400: 'rgb(96 165 250)'
                 },
-                'teal': {
-                    '300': 'rgb(94 234 212)',
-                    '400': 'rgb(45 212 191)'
+                teal: {
+                    300: 'rgb(94 234 212)',
+                    400: 'rgb(45 212 191)'
                 },
             },
         },
