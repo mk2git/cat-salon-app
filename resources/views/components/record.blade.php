783482c3740391822e11186e0c687f38cfe7249a
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-24 18:26:06 +0900

    レスポンシブルデザインに修正

--- resources/views/components/record.blade.php
+++ resources/views/components/record.blade.php
@@ -1,7 +1,9 @@
-<div class="border-8 my-10 w-3/4 mx-auto py-6 px-10">
+<p class="text-right me-10 md:text-base lg:text-xl">{{$content['date']}}</p>
+<div class="border-8 border-teal-700 my-8 sm:w-11/12 md:w-10/12 lg:w-3/4 mx-auto py-md-3 px-md-4 py-lg-6 px-lg-10">
   <h2 class="text-center text-4xl mt-6 mb-4">Cat Salon</h2>
-  <div class="grid grid-cols-2 gap-5">
-    <div class="px-6">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:text-sm lg:text-base">
+    
+    <div class="px-6 pt-2">
       <div class="grid grid-cols-2 gap-5 mb-6">
         <div>
           <p><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;名前：</p>
@@ -37,13 +39,12 @@
         <div class="text-right">その他：</div>
         <div class="col-span-2 border-b text-center">{{$content['others']}}</div>
       </div>
-      <p class="mt-3"><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;担当グルーマーからのメッセージ</p>
+      <p class="mt-5"><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;担当グルーマーからのメッセージ</p>
       <p class="border-2 m-2 p-3">{{$content['message']}}</p>
     </div>
+
     <div class="mt-6">
-      <p class="text-right me-10 text-xl">{{$content['date']}}</p>
-      <hr>
-      <p class="text-center border-b-4 mt-4">料金詳細</p>
+      <p class="text-center border-b-4">料金詳細</p>
       <div class="grid grid-cols-2 mt-4 text-center border-b">
         <div>{{$content['course_name']}}</div>
         <div>&yen;{{$content['course_fee']}}</div>
@@ -54,9 +55,9 @@
           <div>&yen;{{number_format($option->reserve_option->fee)}}</div>
         </div>
       @endforeach
-      <p class="mt-6 text-center w-full"><span class="border-b-2 border-solid border-black inline-block w-1/2 float-right">subTotal：&nbsp;&nbsp;&nbsp;&nbsp;&yen;{{$content['subTotal']}}</span></p><br>
-      <p class="mt-6 text-center w-full"><span class="border-b-2 border-solid border-black inline-block w-1/3 float-right">Tax：&nbsp;&nbsp;&nbsp;&nbsp;&yen;{{$content['onlyTax']}}</span></p><br>
-      <p class="mt-8 text-center w-full"><span class="border-b-4 border-double border-black inline-block w-3/4 float-right text-2xl">Total：&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&yen;{{$content['total']}}</span></p>
+      <p class="mt-6 text-center w-full"><span class="border-b-2 border-solid border-black inline-block w-1/2 float-right me-10">subTotal：&nbsp;&nbsp;&nbsp;&nbsp;&yen;{{$content['subTotal']}}</span></p><br>
+      <p class="mt-6 text-center w-full"><span class="border-b-2 border-solid border-black inline-block w-1/3 float-right me-10">Tax：&nbsp;&nbsp;&nbsp;&nbsp;&yen;{{$content['onlyTax']}}</span></p><br>
+      <p class="mt-8 text-center w-full"><span class="border-b-4 border-double border-black inline-block w-3/4 float-right text-2xl mb-2 me-2">Total：&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&yen;{{$content['total']}}</span></p>
    </div>
   </div>
 </div>
