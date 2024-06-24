f08bc57dd58ecf9e54fa1468986b1d567759ce22
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-24 23:46:44 +0900

    フォントサイズ修正

--- resources/views/components/record-form.blade.php
+++ resources/views/components/record-form.blade.php
@@ -51,7 +51,7 @@
     <input type="text" name="others" class="w-3/4 rounded-md border-b border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value="{{ $recordList['others'] ?? '' }}">
   </div>
   <div>
-    <p><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;担当グルーマーからのメッセージ</p>
+    <p class="text-sm md:text-base"><i class="fa-solid fa-paw"></i>&nbsp;&nbsp;担当グルーマーからのメッセージ</p>
     <textarea name="message" cols="30" rows="10" class="w-full rounded-md border-b border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{ $recordList['message'] ?? '' }}</textarea>
   </div>
 </div>
