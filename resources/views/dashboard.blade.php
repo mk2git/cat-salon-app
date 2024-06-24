3b933c0d4e27df09c4a65678867f8024ccb5d916
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-24 16:39:27 +0900

    レスポンシブルデザインに修正

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,14 +21,16 @@
                         @elseif ($reserve['checkout_status'] == config('reserve.done'))
                             <li class="mb-3">
                                 <a href="{{route('record.create', $reserve['id'])}}" class="hover:text-teal-500">
-                                    {{$reserve['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['user_name']}} <small>様</small>&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['course_name']}} @if (($reserve['option_names']))（オプション：{{$reserve['option_names']}}） @endif
+                                    {{$reserve['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['user_name']}} <small>様</small>&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['course_name']}} @if (($reserve['option_names']))
+                                     <small>（オプション：{{$reserve['option_names']}}）</small>
+                                     @endif
                                 </a>
                                 <span class="text-blue-700 ms-3">お会計済み</span>
                             </li>
                         @else
                             <li class="mb-3">
                                 <a href="{{route('record.create', $reserve['id'])}}" class="hover:text-teal-500">
-                                    {{$reserve['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['user_name']}} <small>様</small>&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['course_name']}} @if (($reserve['option_names']))（オプション：{{$reserve['option_names']}}） @endif
+                                    {{$reserve['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['user_name']}} <small>様</small>&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve['course_name']}} @if (($reserve['option_names']))<small>（オプション：{{$reserve['option_names']}}）</small> @endif
                                 </a>
                             </li>
                         @endif 
@@ -45,7 +47,7 @@
                         @if($reserve_list['user_id'] == Auth::user()->id)   
                         @php $hasReservations = true; @endphp                      
                             <li class="mb-3">
-                                <a href="{{route('reserve.edit', $reserve_list['id'])}}" class="hover:text-teal-400">{{$reserve_list['date']}}&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve_list['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve_list['course_name']}} @if (($reserve_list['option_names']))（オプション：{{$reserve_list['option_names']}}） @endif</a>
+                                <a href="{{route('reserve.edit', $reserve_list['id'])}}" class="hover:text-teal-400">{{$reserve_list['date']}}&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve_list['time']}}〜&nbsp;&nbsp;&nbsp;&nbsp;{{$reserve_list['course_name']}} @if (($reserve_list['option_names']))<small>（オプション：{{$reserve_list['option_names']}}）</small> @endif</a>
                             </li>
                         @endif
                     @endforeach
@@ -59,16 +61,16 @@
      </div>
 
     @can('admin')    
-        <div class="flex flex-wrap justify-center mt-16">
-            <a href="{{route('reserve.index')}}" class="block bg-teal-500 hover:bg-teal-600 text-white py-5 px-10 rounded mx-12 text-2xl">予約受付</a>
-            <a href="{{route('reserveCreate.status')}}" class="block bg-teal-500 hover:bg-teal-600 text-white py-5 px-10 rounded mx-12 text-2xl">予約状況</a>
-            <a href="{{route('checkout.index')}}" class="block bg-teal-500 hover:bg-teal-600 text-white py-5 px-10 rounded mx-12 text-2xl checkout-button"><span class="count">{{$checkout_count}}</span>お会計</a>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-1 mt-16 text-center flex items-center">
+            <a href="{{route('reserve.index')}}" class="block bg-teal-500 hover:bg-teal-600 text-white py-4 rounded mx-12 sm:text-2xl md:text-base lg:text-2xl md:w-7/12 lg:w-7/12">予約受付</a>
+            <a href="{{route('reserveCreate.status')}}" class="block bg-teal-500 hover:bg-teal-600 text-white py-4 rounded mx-12 sm:text-2xl md:text-base lg:text-2xl md:w-7/12 lg:w-7/12">予約状況</a>
+            <a href="{{route('checkout.index')}}" class="block bg-teal-500 hover:bg-teal-600 text-white py-4 rounded mx-12 sm:text-2xl md:text-base lg:text-2xl md:w-7/12 lg:w-7/12 checkout-button"><span class="count">{{$checkout_count}}</span>お会計</a>
         </div>
     @else
-        <div class="flex flex-wrap justify-center mt-16">
-            <a href="{{route('reserve.index')}}" class="block bg-rose-600 hover:bg-rose-700 text-white py-5 px-10 rounded mx-12 text-2xl">予約受付</a>
-            <a href="{{route('record.userRecords', Auth::user()->id)}}" class="block bg-rose-600 hover:bg-rose-700 text-white py-5 px-10 rounded mx-12 text-2xl"><i class="fa-solid fa-list"></i>&nbsp;&nbsp;サロン履歴</a>
-            <a href="{{route('stamp.show', Auth::user()->id)}}" class="block bg-rose-600 hover:bg-rose-700 text-white py-5 px-10 rounded mx-12 text-2xl"><i class="fa-solid fa-stamp"></i>&nbsp;&nbsp;スタンプ</a>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-1 mt-16 text-center flex items-center">
+            <a href="{{route('reserve.index')}}" class="block bg-rose-600 hover:bg-rose-700 text-white py-4 rounded mx-12 sm:text-2xl md:text-base lg:text-2xl md:w-7/12 lg:w-7/12">予約受付</a>
+            <a href="{{route('record.userRecords', Auth::user()->id)}}" class="block bg-rose-600 hover:bg-rose-700 text-white py-4 rounded mx-12 sm:text-2xl md:text-base lg:text-2xl md:w-7/12 lg:w-7/12"><i class="fa-solid fa-list"></i>&nbsp;&nbsp;サロン履歴</a>
+            <a href="{{route('stamp.show', Auth::user()->id)}}" class="block bg-rose-600 hover:bg-rose-700 text-white py-4 rounded mx-12 sm:text-2xl md:text-base lg:text-2xl md:w-7/12 lg:w-7/12"><i class="fa-solid fa-stamp"></i>&nbsp;&nbsp;スタンプ</a>
         </div>
     @endcan
     </div>
--- resources/views/stamp/show.blade.php
+++ resources/views/stamp/show.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
   <x-to-dashboard />
-  <div class="container mt-10 w-1/2 mx-auto">
-    <p class="text-center text-2xl"><i class="fa-solid fa-stamp"></i>&nbsp;&nbsp;スタンプカード</p>
-    <div class="border-4 m-6 px-4 pt-4 pb-20">
+  <div class="container mt-10 sm:w-100 md:w-8/12 lg:w-1/2 sm:m-2 md:mx-auto">
+    <p class="text-center text-base md:text-2xl"><i class="fa-solid fa-stamp"></i>&nbsp;&nbsp;スタンプカード</p>
+    <div class="border-4 m-4 px-4 pt-4 pb-20">
       <x-stamp-card :dates="$dates" />
       <p class="float-end mt-5">{{count($dates)}} / 20</p>
       {{$done_reserves->links()}}
