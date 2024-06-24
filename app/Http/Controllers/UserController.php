7ea578f16bbf524dc05c09b78f99408228c9bd24
mk2git <86880555+mk2git@users.noreply.github.com>
2024-06-24 14:17:27 +0900

    不要なアクションの削除

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,22 +25,6 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show($id)
@@ -138,27 +122,5 @@ class UserController extends Controller
         // dd($content);
         return view('user.show-record', compact('content'));
     }
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
